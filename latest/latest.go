cf24fea42a2f6bc70a79535cd485746bc78a2d56
groob <vrancean+github@gmail.com>
2017-11-30 09:38:12 -0500

    add version checking

---
+++ latest/latest.go
@@ -0,0 +1,104 @@
+package latest
+
+import (
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+func Version(versions ...string) string {
+	if len(versions) == 0 {
+		return ""
+	}
+
+	v := Sorted(versions...)
+	return v[0]
+}
+
+func Sorted(versions ...string) []string {
+	v := byVersion(versions)
+	sort.Sort(v)
+	return v
+}
+
+func ReverseSorted(versions ...string) []string {
+	v := byVersion(versions)
+	sort.Sort(sort.Reverse(v))
+	return v
+}
+
+var r = regexp.MustCompile(`(?P<major>[\d]+)(?P<min>[A-Z])(?P<patch>[\d]+)(?P<fix>[a-z]?)`)
+
+type byVersion []string
+
+func (a byVersion) Len() int      { return len(a) }
+func (a byVersion) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byVersion) Less(i, j int) bool {
+	return a[i] == version(a[i], a[j])
+}
+
+// return the newer of the two
+func version(a, b string) string {
+	if a == b {
+		return a
+	}
+
+	aa := r.FindStringSubmatch(a)
+	bb := r.FindStringSubmatch(b)
+
+	maa, err := strconv.Atoi(aa[1])
+	if err != nil {
+		panic(err)
+	}
+
+	mbb, err := strconv.Atoi(bb[1])
+	if err != nil {
+		panic(err)
+	}
+
+	major := maa > mbb
+	if major {
+		return a
+	} else if maa < mbb {
+		return b
+	}
+
+	minor := aa[2] > bb[2]
+	if minor {
+		return a
+	} else if aa[2] < bb[2] {
+		return b
+	}
+
+	paa, err := strconv.Atoi(aa[3])
+	if err != nil {
+		panic(err)
+	}
+
+	pbb, err := strconv.Atoi(bb[3])
+	if err != nil {
+		panic(err)
+	}
+
+	patch := paa > pbb
+	if patch {
+		return a
+	} else if paa < pbb {
+		return b
+	}
+
+	if len(aa) > len(bb) {
+		// is a re-build with `a-z` at the end.
+		return a
+	}
+
+	if len(aa) == len(bb) && len(aa) == 5 {
+		// both have a re-build letter, compare.
+		fixup := aa[4] > bb[4]
+		if fixup {
+			return a
+		}
+	}
+
+	return b
+}
